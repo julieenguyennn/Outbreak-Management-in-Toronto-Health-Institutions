975d100e0596bec289317fe705824a6a0ac98537
Julie Nguyen <hongan.nguyen@mail.utoronto.ca>
2023-02-02 16:00:06 -0500

    generate simulation

--- scripts/00_simulation.R
+++ scripts/00_simulation.R
@@ -1,2 +1,7 @@
-simulated_data <- tibble("School" = 1:100, "Vaccine 1" = sample(runif(100,min = 0, max = 100), size = 100, replace = TRUE), "Vaccine 2" = sample(runif(100, min = 0, max = 100), size = 100, replace = TRUE))
+simulated_data <-
+  tibble("Setting" = 1:100,
+         "Cases" = floor(runif(100, min=0, max=100)),
+         "Day Began" = sample(seq(as.Date('2020/01/01'), as.Date('2021/01/01'), by="day"), 100, replace = TRUE),
+         "Day Over" = sample(seq(as.Date('2021/01/01'), as.Date('2022/01/01'), by="day"), 100, replace = TRUE))
+
 simulated_data

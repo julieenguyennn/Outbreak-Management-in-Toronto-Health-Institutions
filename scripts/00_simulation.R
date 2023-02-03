420ecbba3f442d9c582ab66fcc3c0511200b2f27
Julie Nguyen <hongan.nguyen@mail.utoronto.ca>
2023-02-02 23:50:24 -0500

    explain simulation

--- scripts/00_simulation.R
+++ scripts/00_simulation.R
@@ -6,10 +6,22 @@
 # Pre-requisites: 
 # - Have questions to be solved for the data
 
-simulated_data <-
-  tibble("Setting" = 1:100,
-         "Cases" = floor(runif(100, min=0, max=100)),
-         "Day Began" = sample(seq(as.Date('2020/01/01'), as.Date('2021/01/01'), by="day"), 100, replace = TRUE),
-         "Day Over" = sample(seq(as.Date('2021/01/01'), as.Date('2022/01/01'), by="day"), 100, replace = TRUE))
+library(tidyverse)
 
-simulated_data
+# Generate a table that portrays the cases in each outbreak type by settings
+
+simulated_data_1 <-
+  tibble("Setting" = sample(c("LTCH","Hospital","Retirement Home","Transitional Care"), 4),
+         "Type" = sample(c("Respiratory", "Enteric"), 4, replace = TRUE),
+         "Cases" = floor(runif(4, min=0, max=100)),
+         )
+simulated_data_1
+
+# Generate a table that has the date outbreak begins and date it's over of each setting by outbreak 
+
+simulated_data_2 <-
+  tibble("Setting" = sample(c("LTCH","Hospital","Retirement Home","Transitional Care"), 4),
+         "Type" = sample(c("Respiratory", "Enteric"), 4, replace = TRUE),
+         "Day Began" = sample(seq(as.Date('2020/01/01'), as.Date('2021/01/01'), by="day"), 4, replace = TRUE),
+         "Day Over" = sample(seq(as.Date('2021/01/01'), as.Date('2022/01/01'), by="day"),4, replace = TRUE))
+simulated_data_2
